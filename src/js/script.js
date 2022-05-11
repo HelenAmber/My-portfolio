4a2dc03a769086c66588c4b7d6330b83a55a3770
Elena <elena.nisman85@gmail.com>
2022-05-11 16:07:45 +0300

    added chevron-up

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,8 @@ const hamburger = document.querySelector('.humburger'),
       menu = document.querySelector('.menu'),
       close = document.querySelector('.menu__close'),
       counters = document.querySelectorAll('.myskills__progress__interest'),
-      levels = document.querySelectorAll('.myskills__progress__level');
+      levels = document.querySelectorAll('.myskills__progress__level'),
+      pageUp = document.querySelector('.page_up');
 
 hamburger.addEventListener('click', () => {
      menu.classList.add('active');
@@ -16,4 +17,12 @@ counters.forEach((item, i) => {
      levels[i].style.width = item.innerHTML;
 });
 
+function showChevronUp(){
+  if (window.pageYOffset <= document.documentElement.clientHeight){
+        pageUp.hidden = true;
+     } else {
+        pageUp.hidden = false; 
+     }
+}
 
+window.addEventListener('scroll', showChevronUp);
